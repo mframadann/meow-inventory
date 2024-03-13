3f807b58b842f50519e51dafd5a64a3390738765
Muhamad Firly Ramadan <dev.ramadann@gmail.com>
2024-03-13 22:58:10 +0700

    feat(product-category): Read & Delete Product Category

---
+++ app/Filament/Resources/Product/ProductCategoryResource.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Resources\Product;
+
+use App\Filament\Resources\Product\ProductCategoryResource\Pages;
+use App\Filament\Resources\Product\ProductCategoryResource\RelationManagers;
+use App\Models\ProductCategory;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductCategoryResource extends Resource
+{
+    protected static ?string $model = ProductCategory::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-inbox-stack';
+    protected static ?string $navigationGroup = "Product";
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make("name"),
+                TextColumn::make("products_count")->counts("products"),
+                TextColumn::make("created_at")->datetime(),
+                TextColumn::make("updated_at")->datetime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageProductCategories::route('/'),
+        ];
+    }
+}
